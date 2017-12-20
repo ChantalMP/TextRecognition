0699058381e027e188ee214f45ca34f1be2aecf8
Chantal <chantal.pellegrini@gmail.com>
2017-12-20 20:08:42 +0100

    project creation

---
+++ app/src/main/java/cp/textrecognition/SelectVersion.java
@@ -0,0 +1,13 @@
+package cp.textrecognition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectVersion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_version);
+    }
+}
